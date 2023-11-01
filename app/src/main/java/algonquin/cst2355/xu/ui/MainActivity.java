d65e8441045ce563d155a758a56eb88ce41c0630
Hanliying Xu <xu000295@algonquinlive.com>
2023-11-01 19:10:30 -0400

    generate javadoc

--- app/src/main/java/algonquin/cst2355/xu/ui/MainActivity.java
+++ app/src/main/java/algonquin/cst2355/xu/ui/MainActivity.java
@@ -1,114 +1,107 @@
 package algonquin.cst2355.xu.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.content.Intent;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-
-import algonquin.cst2355.xu.R;
-import algonquin.cst2355.xu.data.MainViewModel;
 import algonquin.cst2355.xu.databinding.ActivityMainBinding;
 
+/** This class is the starting point of the application
+ * @author Hanliying Xu
+ * @version 1.0
+ */
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "MainActivity";
-    private ActivityMainBinding variableBinding;
-    private MainViewModel model;
+
+    /**
+     * @param savedInstanceState If the activity is being re-initialized after previously being
+     *        shut down then this Bundle contains the data it most recently
+     *        supplied in onSaveInstanceState(Bundle). Note: Otherwise it is null.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        setupLoginButton();
-        loadSavedEmailAddress();
+        binding.loginButton.setOnClickListener(click -> {
+            String userInput = binding.passwordText.getText().toString();
 
-        Log.w(TAG, "onCreate: The activity is being created.");
-    }
-    private void setupLoginButton() {
-        Button loginButton = findViewById(R.id.buttonLogin);
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String emailAddress = getEmailAddressFromEditText();
-                saveEmailAddressToSharedPreferences(emailAddress);
-                navigateToSecondActivity(emailAddress);
+            boolean hasUpperAndLowerCase = containsUpperAndLowerCase(userInput);
+            boolean hasDigit = containsDigit(userInput);
+            boolean hasSpecialChar = containsSpecialCharacters(userInput);
+
+            String message = "You shall not pass!";
+            boolean hasMissing = false;
+
+            if (!hasUpperAndLowerCase || !hasDigit || !hasSpecialChar) {
+                //message += "missing upper and lower case letter";
+                hasMissing = true;
+            }
+            if (!hasDigit) {
+                //message += " missing digits";
+                hasMissing = true;
+            }
+            if (!hasSpecialChar) {
+                //message += " missing special characters *, #, ?, ";
+                hasMissing = true;
+            }
+
+            if (hasMissing) {
+                binding.responseText.setText(message);
+            } else {
+                binding.responseText.setText("Your password is complex enough.");
             }
         });
     }
 
-    private String getEmailAddressFromEditText() {
-        EditText emailEditText = findViewById(R.id.editTextEmail);
-        return emailEditText.getText().toString();
+    /** This function checks str and returns true if str
+     *  has an upper and lower case letter in it
+     * @param str a String str to store user input
+     * @return Returns true if str has upper and lower case, otherwise false
+     */
+    boolean containsUpperAndLowerCase(String str) {
+        boolean foundUpper = false;
+        boolean foundLower = false;
+
+        for (int i = 0; i < str.length(); i++) {
+            //get each character in the string
+            char c = str.charAt(i);
+            if (Character.isLowerCase(c)) {
+                foundLower = true;
+            } else if (Character.isUpperCase(c)) {
+                foundUpper = true;
+            }
+        }
+        //after looked at every character
+        return foundLower && foundUpper;
     }
 
-    private void saveEmailAddressToSharedPreferences(String emailAddress) {
-        // Save the email address to SharedPreferences
-        // Replace "MyData" with your SharedPreferences file name
-        getSharedPreferences("MyData", MODE_PRIVATE)
-                .edit()
-                .putString("LoginName", emailAddress)
-                .apply();
+    /** This function checks str and returns true if str
+     *  has digits in it
+     * @param str a String str to store user input
+     * @return Returns true if str has digits, otherwise false
+     */
+    boolean containsDigit(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (Character.isDigit(c)) {
+                return true;
+            }
+        }
+        return false;
     }
 
-    private void navigateToSecondActivity(String emailAddress) {
-        Intent nextPage = new Intent(MainActivity.this, algonquin.cst2355.xu.SecondActivity.class);
-        nextPage.putExtra("EmailAddress", emailAddress);
-        startActivity(nextPage);
+    /** This function checks str and returns true if str
+     *  has special characters in it
+     * @param str a String str to store user input
+     * @return Returns true if str has special characters, otherwise false
+     */
+    boolean containsSpecialCharacters(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c == '*' || c == '#' || c == '?') {
+                return true;
+            }
+        }
+        return false;
     }
-
-    private void loadSavedEmailAddress() {
-        String emailAddress = getEmailAddressFromSharedPreferences();
-        setEmailAddressToEditText(emailAddress);
-    }
-
-    private String getEmailAddressFromSharedPreferences() {
-        // Load the saved email address from SharedPreferences
-        // Replace "MyData" with your SharedPreferences file name
-        return getSharedPreferences("MyData", MODE_PRIVATE)
-                .getString("LoginName", "");
-    }
-
-    private void setEmailAddressToEditText(String emailAddress) {
-        EditText emailEditText = findViewById(R.id.editTextEmail);
-        emailEditText.setText(emailAddress);
-    }
-
-// Other lifecycle methods (onStart, onResume, onPause, onStop, onDestroy) can remain unchanged.
-
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.w(TAG, "onStart: The activity is about to become visible.");
-    }
-    protected void onResume() {
-        super.onResume();
-        Log.w(TAG, "onResume: The activity has become visible and is now in the foreground.");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-        Log.w(TAG, "onPause: The activity is partially obscured or going into the background.");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-
-        Log.w(TAG, "onStop: The activity is no longer visible.");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        Log.w(TAG, "onDestroy: The activity is being destroyed.");
-    }}
-// Saving email address
-
+}
